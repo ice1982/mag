13baf00e003856376dabd56db74692478199582c
ice1982 <ice1982@list.ru>
2017-07-25 18:09:08 +0300

    first commit

---
+++ modules/start/start.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\start;
+
+/**
+ * start module definition class
+ */
+class start extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\start\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
